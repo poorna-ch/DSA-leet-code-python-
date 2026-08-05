ffd3eebcf4780d3b5241af87d97aa97a7f46c2cd
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-05 08:43:00 +0530

    Time: 7 ms (81.26%) | Memory: 12.5 MB (18.10%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,10 +1,19 @@
 class Solution(object):
+    # def isPowerOfThree(self, n):
+    #     if n<=0:
+    #       return False
+    #     while n%3==0:
+    #         n=n//3
+    #     if n==1:
+    #         return True
+    #     else:
+    #         return False
     def isPowerOfThree(self, n):
         if n<=0:
-          return False
-        while n%3==0:
-            n=n//3
-        if n==1:
-            return True
-        else:
             return False
+        if n==3 or n==1:
+            return True
+        if n%3!=0:
+            return False
+         #recursive case
+        return self.isPowerOfThree(n//3)
