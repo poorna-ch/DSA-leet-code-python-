6ba987fbd66577d0794502954c2b63cb62051bfe
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-05 08:22:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.01%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,10 +1,20 @@
 class Solution(object):
+    # def isPowerOfTwo(self, n):
+    #     # if n<=0:
+    #     #     return False
+    #     # while n%2==0:
+    #     #     n=n//2
+    #     # if n==1:
+    #     #     return True 
+    #     # else :
+    #     #     return False
+
     def isPowerOfTwo(self, n):
         if n<=0:
             return False
-        while n%2==0:
-            n=n//2
         if n==1:
-            return True 
-        else :
+            return True
+        if n%2!=0:
             return False
+        #recursive case
+        return self.isPowerOfTwo(n//2)
