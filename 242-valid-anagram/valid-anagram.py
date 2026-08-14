96e06775fc751bf138b637722ced4e3c465db9be
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-14 10:55:29 +0530

    Time: 25 ms (32.00%) | Memory: 13.4 MB (28.89%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+        sort_s=sorted(s)
+        sort_t=sorted(t)
+        if sort_s==sort_t:
+            return True
+        return False
+        
