5e985e1a126f4bbb40c091e4467b2472a4145a23
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-17 06:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (47.06%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        i,count=len(s)-1,0
+        while s[i]==" ":
+            i-=1
+        while i>=0 and s[i]!=" ":
+            count+=1
+            i-=1
+        return count
+        
+
+
+      
