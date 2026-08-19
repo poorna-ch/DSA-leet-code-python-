6766a299742042667dd6b6c32cae5f80553edbf7
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-19 20:20:48 +0530

    Time: 11 ms (45.96%) | Memory: 12.3 MB (57.61%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,22 +1,19 @@
 class Solution(object):
     def romanToInt(self, s):
-        letters = {
+        letters = {     #store in dictionary
             "I": 1,
             "V": 5,
             "X": 10,
             "L": 50,
-            "C": 100,
+             "C": 100,
             "D": 500,
             "M": 1000
         }
-
-        n = len(s)
-        result = 0
-
-        for i in range(n):
-            if i < n - 1 and letters[s[i]] < letters[s[i + 1]]:
-                result -= letters[s[i]]
+        n= len(s)
+        result=0
+        for i in range(0,n):
+            if i<n-1 and letters[s[i]]<letters[s[i+1]]:
+                 result-=letters[s[i]]
             else:
-                result += letters[s[i]]
-
-        return result
+                 result+=letters[s[i]]
+        return result       
