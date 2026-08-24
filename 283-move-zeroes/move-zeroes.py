6509a12b3dddd8bfc692413db82e24063e7459dc
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-24 07:10:07 +0530

    Time: 7 ms (55.80%) | Memory: 13.6 MB (50.10%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        for i in nums:
-            if i==0:
-                nums.remove(i)
-                nums.append(i)
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
         
