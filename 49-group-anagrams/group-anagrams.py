78b6c9a82fbdb1cc259fc9494ee03ec8bfbbc78a
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-08-26 09:48:21 +0530

    Time: 27 ms (27.98%) | Memory: 18.1 MB (13.05%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagram_dict={}
+        for s in strs:
+            count=[0]*26
+            for ch in s:
+                count[ord(ch)-ord('a')]+=1
+            key=tuple(count)
+            if key not in anagram_dict:
+                anagram_dict[key]=[]
+            anagram_dict[key].append(s)
+        return anagram_dict.values()
+        
