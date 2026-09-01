b94d99dd98ef2401a75914c5f299351e76141624
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-09-01 06:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (37.67%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -4,11 +4,12 @@ class Solution(object):
         low=0
         high=n-1
         while low<=high:
-            mid=(low+high)//2
+            mid=(low+high)/2
             if nums[mid]==target:
                 return mid
             elif nums[mid]<target:
                 low=mid+1
-            else :
+            else:
                 high=mid-1
-        return -1
+        return-1
+        
