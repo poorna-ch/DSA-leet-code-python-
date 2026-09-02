ee17f55bafde9b67d649be950642009d40ed9e54
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-09-02 06:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (66.33%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def searchInsert(self, nums, target):
         n=len(nums)
-        lb=n
         low=0
         high=n-1
+        lb=n
         while low<=high:
             mid=(low+high)/2
             if nums[mid]>=target:
@@ -12,3 +12,4 @@ class Solution(object):
             else:
                 low=mid+1
         return lb
+        
