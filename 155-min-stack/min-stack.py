5f01eb9834d79e3b9dd8f5a5e6dad4ff6cca155f
Poorna Chandra Tejaswi.L <152604561+poorna-ch@users.noreply.github.com>
2026-09-22 07:42:39 +0530

    Time: 151 ms (76.95%) | Memory: 24.7 MB (39.35%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -9,13 +9,16 @@ class MinStack(object):
         self.stack.append(val)
         val=min(val,self.minstack[-1] if self.minstack else val)
         self.minstack.append(val)
+        
 
     def pop(self):
         self.stack.pop()
         self.minstack.pop()
+        
 
     def top(self):
         return self.stack[-1]
+        
 
     def getMin(self):
         return self.minstack[-1]
